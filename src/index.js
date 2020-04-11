b3d702a12f9ef453ea15fe18be3821673a119f0f
James <jameselliottmillington@gmail.com>
2020-04-11 13:09:00 -0400

    yeee

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-function draw(){
+
 var canvas = document.getElementById("can");
 var ctx = canvas.getContext("2d");
-ctx.beginPath();
-ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-ctx.stroke();}
+
+function draw(){
+    ctx.beginPath();
+    ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+
+function x(x, y){
+    var x = 'X';
+    ctx.font = "48px serif";
+    ctx.fillText(x, 100, 100);
+}
+
 draw();
+x();
