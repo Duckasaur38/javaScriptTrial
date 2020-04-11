54ca86012ba9b0609b9e5a85eafa40f64bdd3c35
Duckasaur <zshleifie38@gmail.com>
2020-04-11 11:24:40 -0400

    fixed merge conflict againcleaclear

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("GOOOOD MORNING")
+function draw(){
+var canvas = document.getElementById("can");
+var ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+ctx.stroke();}
+draw();
