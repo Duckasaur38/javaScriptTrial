424dfe7485015ef32b27a64f8713b15e91c21c4f
Duckasaur <zshleifie38@gmail.com>
2020-04-11 18:43:05 -0400

    try clicking on the squares?

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
-var canvas = document.getElementById("can");
+// sets up a rectangle object (there is a built in version which is apparently janky)
+class MyRect {
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.width = w;
+        this.height = h;
+        this.contains = function (x, y) {
+            return this.x <= x && x <= this.x + this.width && this.y <= y && y <= this.y + this.height;
+        };
+        this.draw = function (ctx) {
+            ctx.rect(this.x, this.y, this.width, this.height);
+        };
+    }
+}
+
+//starts an array of rectangles
+let rectangles = [];
+//goes through and sets coordinates of 9 rectangles (these will be the grid boxes)
+for(let i=0;i<3;i++){
+    for(let j=0;j<3;j++){
+        rectangle = new MyRect(51+(i*50),75+(j*50),50,50)
+    rectangles.push(rectangle);
+    console.log(rectangle.x);
+}
+}
+//prints the rectangles
+for(let i=0;i<rectangles.length;i++){
+    console.log(rectangles[i]);
+}
+// draws grid
+function grid(){
+    var canvas = document.getElementById("can");
 var ctx = canvas.getContext("2d");
 
-function circle(x,y){
-    ctx.beginPath();
-    ctx.arc(45+(50*x),50+(50*y), 20, 0, 2 * Math.PI);
-    // x, y, diameter, ???, arc size in radians
-}
-function grid(){
     ctx.beginPath();
     //v1
     ctx.moveTo(95,50);
@@ -23,18 +49,39 @@ function grid(){
     ctx.stroke();
 }
 
-grid();
-circle(.5,.5);
-circle(1.5,1.5);
-circle(2.5,2.5);
+// methods to draw x's and o's takes (x,y) where each are 0,1,2
+function ex(x, y){
+    
+    var canvas = document.getElementById("can");
+    var ctx = canvas.getContext("2d");
+        ctx.font = "48px serif";
+        ctx.fillText("X", 50+(50*x), 95+(50*y));
+    }
 
-var canvas = document.getElementById("can");
-var ctx = canvas.getContext("2d");
-
-function x(x, y){
-    var x = 'X';
-    ctx.font = "48px serif";
-    ctx.fillText(x, 100, 100);
+function o(x, y){
+    
+    var canvas = document.getElementById("can");
+    var ctx = canvas.getContext("2d");
+        ctx.font = "48px serif";
+        ctx.fillText("O", 50+(50*x), 95+(50*y));
 }
-x();
-ctx.stroke();
+function buildBtns() {
+    for (var i = 0; i < rectangles.length; i++){
+       var btn = document.createElement("button");
+       document.body.appendChild(btn);
+       btn.setAttribute("id",i.toString()+"btn")
+    document.getElementById(i.toString()+"btn").style.padding = '25px 25px';
+    document.getElementById(i.toString()+"btn").style.position = 'absolute';
+    document.getElementById(i.toString()+"btn").style.background= 'none';
+    document.getElementById(i.toString()+"btn").style.border = 'none';
+    document.getElementById(i.toString()+"btn").style.left = rectangles[i].x.toString()+'px';
+    document.getElementById(i.toString()+"btn").style.top = rectangles[i].y.toString()+'px';
+    //    btn.setAttribute('coords',rectangles[i].x.toString(),rectangles[i].y.toString(),(rectangles[i].x+50).toString(),(rectangles[i].y+50).toString());
+    //    btn.addEventListener("click",squareClicked(i));
+    }
+}
+function squareClicked(square){
+console.log("YAY"+square.toString());
+}
+buildBtns();
+grid();
