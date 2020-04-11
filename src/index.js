6f02d61d8179ebc371c2f586fbe30098f3be88d2
Duckasaur <zshleifie38@gmail.com>
2020-04-11 13:09:48 -0400

    added html?

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
-function draw(){
-var canvas = document.getElementById("can");
-var ctx = canvas.getContext("2d");
-ctx.beginPath();
-ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+function startCanvas(){
+    
+}
+function circle(x,y){
+    var canvas = document.getElementById("can");
+    var ctx = canvas.getContext("2d");
+
+    ctx.beginPath();
+
+    ctx.arc(45+(50*x),50+(50*y), 20, 0, 2 * Math.PI);
+// x, y, diameter, ???, arc size in radians
 ctx.stroke();}
-draw();
+function grid(){
+    var canvas = document.getElementById("can");
+    var ctx = canvas.getContext("2d");
+
+    ctx.beginPath();
+//v1
+    ctx.moveTo(95,50);
+    ctx.lineTo(95,200);
+//v2
+    ctx.moveTo(145,50);
+    ctx.lineTo(145,200);
+//h1
+    ctx.moveTo(45,100);
+    ctx.lineTo(195,100);
+//h2
+    ctx.moveTo(45,150);
+    ctx.lineTo(195,150);
+    ctx.stroke();
+}
+startCanvas();
+grid();
+circle(.5,.5);
+circle(1.5,1.5);
+circle(2.5,2.5);
