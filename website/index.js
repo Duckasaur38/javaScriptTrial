d3b61bf765456c71f9930379ad9ded9f2bb93df5
Duckasaur38 <62815829+Duckasaur38@users.noreply.github.com>
2020-04-21 21:08:55 -0400

    Update index.js

--- website/index.js
+++ website/index.js
@@ -1,21 +1,13 @@
+Skip to content
+ Duckasaur38 / javaScriptTrial
+Code Issues 0 Pull requests 0 Projects 0 Actions Wiki Security 0 Pulse Community
+javaScriptTrial/src/index.js /
+@Duckasaur38 Duckasaur38 Update index.js
+68d4a73 4 minutes ago
+@Duckasaur38@JimmyJames22
+113 lines (108 sloc)  4.23 KB
+  
 
-// sets up a rectangle object (there is a built in version which is apparently janky)
-class MyRect {
-    constructor(x, y, w, h) {
-        this.x = x;
-        this.y = y;
-        this.width = w;
-        this.height = h;
-        this.contains = function (x, y) {
-            return this.x <= x && x <= this.x + this.width && this.y <= y && y <= this.y + this.height;
-        };
-        this.draw = function (ctx) {
-            ctx.rect(this.x, this.y, this.width, this.height);
-        };
-    }
-}
-
-//starts an array of rectangles
 let rectangles = [];
 //goes through and sets coordinates of 9 rectangles (these will be the grid boxes)
 for(let i=0;i<3;i++){
@@ -24,136 +16,98 @@ for(let i=0;i<3;i++){
         rectangles.push(rectangle);
         console.log(rectangle.x);
       }
+
 }
-//prints the rectangles
-for(let i=0;i<rectangles.length;i++){
-    console.log(rectangles[i]);
+function buildBtns() {
+  for (var i = 0; i < rectangles.length; i++) {
+    var btn = document.createElement("button"); //creates button
+    btn.setAttribute("id", i.toString() + "btn"); //gives the button id : ibtn (where i is for loop variable)
+    document.body.appendChild(btn); // puts button in body of html
+    document.getElementById(i.toString() + "btn").style.padding = "75px 75px"; //sets size
+    document.getElementById(i.toString() + "btn").style.position = "absolute"; // says to give absolute coordinates
+    document.getElementById(i.toString() + "btn").style.left =
+      rectangles[i].x.toString() + "px"; // x pos
+    document.getElementById(i.toString() + "btn").style.top =
+      rectangles[i].y.toString() + "px"; // ypos
+    document.getElementById(i.toString() + "btn").style.background = "none"; //no background
+    document.getElementById(i.toString() + "btn").style.border = "none"; //no border
+    //btn.setAttribute('coords',rectangles[i].x.toString(),rectangles[i].y.toString(),(rectangles[i].x+50).toString(),(rectangles[i].y+50).toString());
+    //btn.addEventListener("click",squareClicked(i));
+    document.getElementById(i.toString() + "btn").onclick = function () {
+      rectangles[i].containsX = true;
+    };
+  }
 }
+
 // draws grid
-function grid(){
-    var canvas = document.getElementById("can");
-    var ctx = canvas.getContext("2d");
-    ctx.beginPath();
-    //v1
-    ctx.moveTo(95,50);
-    ctx.lineTo(95,200);
-    //v2
-    ctx.moveTo(145,50);
-    ctx.lineTo(145,200);
-    //h1
-    ctx.moveTo(45,100);
-    ctx.lineTo(195,100);
-    //h2
-    ctx.moveTo(45,150);
-    ctx.lineTo(195,150);
-    ctx.stroke();
+function ex(x, y) {
+  var canvas = document.getElementById("can");
+  var ctx = canvas.getContext("2d");
+  ctx.font = "48px serif";
+  ctx.fillText("X", 50 + 50 * x, 95 + 50 * y);
+  ctx.stroke();
 }
-// methods to draw x's and o's takes (x,y) where each are 0,1,2
-function ex(x, y){
 
-    var canvas = document.getElementById("can");
-    var ctx = canvas.getContext("2d");
-        ctx.font = "48px serif";
-        ctx.fillText("X", 50+(50*x), 95+(50*y));
-    }
-
-function o(x, y){
-
-    var canvas = document.getElementById("can");
-    var ctx = canvas.getContext("2d");
-        ctx.font = "48px serif";
-        ctx.fillText("O", 50+(50*x), 95+(50*y));
+function o(x, y) {
+  var canvas = document.getElementById("can");
+  var ctx = canvas.getContext("2d");
+  ctx.font = "48px serif";
+  ctx.fillText("O", 50 + 50 * x, 95 + 50 * y);
+  ctx.stroke();
 }
+
 //this method is an ass. it basically goes through and makes a button for each of the boxes
 function buildBtns() {
-    for (var i = 0; i < rectangles.length; i++){
-      var btn = document.createElement("button"); //creates button
-      document.body.appendChild(btn); // puts button in body of html
-      btn.setAttribute("id",i.toString()+"btn") //gives the button id : ibtn (where i is for loop variable)
-      document.getElementById(i.toString()+"btn").style.padding = '25px 25px'; //sets size
-      document.getElementById(i.toString()+"btn").style.position = 'absolute'; // says to give absolute coordinates
-      document.getElementById(i.toString()+"btn").style.background= 'none'; //no background
-      document.getElementById(i.toString()+"btn").style.border = 'none'; //no border
-      document.getElementById(i.toString()+"btn").style.left = rectangles[i].x.toString()+'px'; // x pos
-      document.getElementById(i.toString()+"btn").style.top = rectangles[i].y.toString()+'px'; // ypos
-      document.getElementById(i.toString()+"btn").onclick = function() { squareClicked(this.id);}
-    }
+  for (var i = 0; i < rectangles.length; i++) {
+    var btn = document.createElement("button"); //creates button
+    document.body.appendChild(btn); // puts button in body of html
+    btn.setAttribute("id", i.toString() + "btn"); //gives the button id : ibtn (where i is for loop variable)
+    document.getElementById(i.toString() + "btn").style.padding = "25px 25px"; //sets size
+    document.getElementById(i.toString() + "btn").style.position = "absolute"; // says to give absolute coordinates
+    document.getElementById(i.toString() + "btn").style.background = "none"; //no background
+    document.getElementById(i.toString() + "btn").style.border = "none"; //no border
+    document.getElementById(i.toString() + "btn").style.left =
+      rectangles[i].x.toString() + "px"; // x pos
+    document.getElementById(i.toString() + "btn").style.top =
+      rectangles[i].y.toString() + "px"; // ypos
+    document.getElementById(i.toString() + "btn").onclick = function () {
+      squareClicked(this.id);
+    };
+  }
 }
- var turn = 0;
- var squVals = new Array(9).fill(0);
- 
-function checkWin(num){
-    var checkAr = [];
-    for(let i =0; i<9;i++){
-        if(Math.floor(i/3)== Math.floor(num/3) && squVals[num] == squVals[i] && i!=num){
-            checkAr.push(i);
-        }
+var turn = 0;
+var squVals = new Array(9).fill(0);
+// checks if the game is over
+function checkWin(num) {
+  if (
+    (squVals[num] == squVals[(num + 3) % 9] &&
+      squVals[num] == squVals[(num + 6) % 9]) ||
+    (squVals[4] != 0 &&
+      ((squVals[0] == squVals[4] && squVals[4] == squVals[8]) ||
+        (squVals[6] == squVals[4] && squVals[4] == squVals[2])))
+  ) {
+    console.log("player " + squVals[num] + " wins!");
+  }
+  if (squVals[0] != 0) {
+    if (squVals[0] == squVals[1] && squVals[1] == squVals[2]) {
+      console.log("player " + squVals[num] + " wins vertically in column 1!");
     }
-    if(checkAr.length == 2){
-        checkAr.push[num];
-        console.log("player "+squVals[num]+" wins!");
-        return;
+  }
+  if (squVals[3] != 0) {
+    if (squVals[3] == squVals[4] && squVals[4] == squVals[5]) {
+      console.log("player " + squVals[num] + " wins vertically in column 2!");
     }
-    else{
-        checkAr=[];
+  }
+  if (squVals[6] != 0) {
+    if (squVals[6] == squVals[7] && squVals[7] == squVals[8]) {
+      console.log("player " + squVals[num] + " wins vertically in column 3!");
     }
-    for(let i =0; i<9;i++){
-        if(num%3 == i%3 && squVals[num] == squVals[i] && i!=num){
-            checkAr.push(i);
-        }
-    }
-    if(checkAr.length == 2){
-        checkAr.push[num];
-        console.log("player "+squVals[num]+" wins!");
-        return;
-    }
-    else{
-        checkAr=[];
-    }
-    if(squVals[num]==squVals[4]){
-        for(x=0; x<2; x++){
-            var s1, s2;
-            if(x==0){
-                s1 = 8;
-                s2 = 0;
-            } 
-            else {
-              s1 = 6;
-              s2 = 2;
-            }
-            if(squVals[s1] == squVals[num] && squVals[s2] == squVals[num]){
-                console.log("Player "+squVals[num]+" wins!")
-            } else {
-                return;
-            }
-        }
-    }
-    else {
-        return;
-    }
-}
-
-function checkDraw(){
-    var draw = true;
-    for(let i=0;i<10;i++){
-        if(squVals[i] == 0){
-            draw = false;
-            break;
-        }
-        if(draw){
-                console.log("DRAW!")
-            }
-      }
-    }
-function squareClicked(square){
-    document.getElementById(square).setAttribute('disabled','true');
-    turn++;
-    turn %=2;
-    var squNum = parseInt(square);
-    console.log(squNum);
-    if(turn == 0){
-        ex(Math.floor(squNum/3),squNum%3);
-        squVals[squNum] = 1;
+  }
+  var draw = true;
+  for (let i = 0; i < 10; i++) {
+    if (squVals[i] == 0) {
+      draw = false;
+      break;
     }
     else{
         o(Math.floor(squNum/3),squNum%3);
@@ -162,5 +116,19 @@ function squareClicked(square){
     checkWin(squNum);
     checkDraw();
 }
-buildBtns();
+setUpRects();
 grid();
+buildBtns();
+}
+© 2020 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
